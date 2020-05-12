38baa4c0526a41538291fed1c3ebe1778ae45ac9
dotrider <56666461+dotrider@users.noreply.github.com>
2020-05-12 14:45:00 -0700

    adding project description

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -22,9 +22,30 @@ const Projects = () => {
             <div className='contgrid'>
                 <div>
                     <img alt={ns} className='projectPic' src={ns}/>
+                    <div className='project-description'>
+                        <div className='project-name ns'>
+                            <h3 className='h3'>NoSponsors</h3>
+                        </div>
+                        <p>NoSponsors is a Full Stack Application I built as a Student during my time in DevMountain.
+                            It features a forum where off-Road vehicle enthusiasts can collaborate on their vehicle builds and
+                            an integrated store where they can buy products to support the off-road community.
+                        </p>
+                    </div>
+                    <span className='live-site'>Live Site</span>  
                 </div>
+
                 <div>
                     <img alt={color} className='projectPic' src={color}/>
+                    <div className='project-description'>
+                        <div className='project-name colorizer'>
+                            <h3 className='h3'>Colorizer</h3>
+                        </div>
+                        <p>A collaborative drawing app built as a group project with my Teammate Jared Baur.
+                            Colorizer allows multiple users to draw on the same Canvas and export their final art
+                            in a png file. A great app to have fun with friends and family.
+                        </p>
+                    </div>
+                    <span className='live-site'>Live Site</span>  
                 </div>
             </div>
         
