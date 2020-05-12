5298586abb51040078048c16ae8bf2d02ffed8f9
dotrider <56666461+dotrider@users.noreply.github.com>
2020-05-11 19:43:12 -0700

    Updates

---
+++ precache-manifest.17f17702c006f2994fc37a34e9973240.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b36ac4e823cb7dec1d52a317ae8f61",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "15d4a5876ea8ea5672c6",
+    "url": "/myportfolio/static/css/main.196feb14.chunk.css"
+  },
+  {
+    "revision": "1df23624ece1f6753619",
+    "url": "/myportfolio/static/js/2.1e9be9c9.chunk.js"
+  },
+  {
+    "revision": "ccc502121842bd2ec2d999aa7d823359",
+    "url": "/myportfolio/static/js/2.1e9be9c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d4a5876ea8ea5672c6",
+    "url": "/myportfolio/static/js/main.fac5eeef.chunk.js"
+  },
+  {
+    "revision": "5c290f7833f29dc60efa",
+    "url": "/myportfolio/static/js/runtime-main.cf9178a5.js"
+  },
+  {
+    "revision": "6a97cef59c58a147a7d8926ccdf87815",
+    "url": "/myportfolio/static/media/Miranda_WPX17_20200403_.6a97cef5.pdf"
+  },
+  {
+    "revision": "eb0632af8ac66a392cacdca5bbc191c9",
+    "url": "/myportfolio/static/media/color.eb0632af.png"
+  },
+  {
+    "revision": "f2f500780aec9ace0d1314c16a331806",
+    "url": "/myportfolio/static/media/ns.f2f50078.png"
+  }
+]);
