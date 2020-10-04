57c07e7b25a54ad70550dcaba8fbffe6ff4a0daf
dotrider <56666461+dotrider@users.noreply.github.com>
2020-10-04 14:57:21 -0700

    making changes

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import {useSpring, animated} from 'react-spring'
+// import {useSpring, animated} from 'react-spring'
+import { MDBAnimation } from "mdbreact";
 import Nav from '../Nav/Nav';
 import './Landing.css';
 
 const Landing = () => {
-    const fade = useSpring({from:{ opacity: 0}, to:{opacity: 1}, config: { duration: 1500 }})
     const [ user, setUser ] = useState({
         firstName: 'Pablo',
         lastName: 'Miranda',
@@ -16,14 +16,14 @@ const Landing = () => {
     return(
         <section id='home' className='landing'>
             <Nav/>
-           <animated.div style={fade}>
+           <MDBAnimation type='fadeIn'>
                 <div className='mainText'>
                     <div><h1 className='name'>{firstName} <span className='bold'>{lastName}</span></h1>
                         <h1 className='title'> {occupation[0]}
                         <span className='bold'> <br/> {occupation[1]}</span></h1>
                     </div>
                 </div>
-           </animated.div>
+           </MDBAnimation>
         </section>
     )
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Projects.css';
 import data from '../../data'
+import { MDBAnimation } from "mdbreact";
 
 const Projects = () => {
     const sectionName = 'Projects'
@@ -8,28 +9,30 @@ const Projects = () => {
 
     return(
         <section id='projects'>
-              <h2 className='headings'>{sectionName}</h2>
-                <div className='contgrid'>
-                    {data.map(({name, image, technologies, liveSite, description, id}, i) => 
-                        <div key={id}>
-                           <img alt={name} className='projectPic' src={image}/>
-                            <div className='project-description'>
-                                <div className='project-name ns'>
-                                    <h3 className='h3'>{name}</h3>
+                  <h2 className='headings'>{sectionName}</h2>
+                    <div className='contgrid'>
+                        {data.map(({name, image, technologies, liveSite, description, id}, i) => 
+            //   <MDBAnimation reveal type='fadeIn'>
+                            <div key={id}>
+                               <img alt={name} className='projectPic' src={image}/>
+                                <div className='project-description'>
+                                    <div className='project-name ns'>
+                                        <h3 className='project-name'>{name}</h3>
+                                    </div>
+                                    <p>{description}</p>
                                 </div>
-                                <p>{description}</p>
+                                <p className='techs'>
+                                    {technologies.map((techs, i, arr) =>
+                                    arr.length-1 === i? <span key={i}> {techs}</span> : <span key={i}> {techs} |</span>
+                                    )}
+                                </p>
+                                <a href={liveSite} target="_blank" rel="noopener noreferrer">
+                                    <span className='live-site'>{btnText}</span>
+                                </a>  
                             </div>
-                            <p className='techs'>
-                                {technologies.map((techs, i, arr) =>
-                                arr.length-1 === i? <span key={i}> {techs}</span> : <span key={i}> {techs} |</span>
-                                )}
-                            </p>
-                            <a href={liveSite} target="_blank" rel="noopener noreferrer">
-                                <span className='live-site'>{btnText}</span>
-                            </a>  
-                        </div>
-                    )}
-                </div>      
+                        // </MDBAnimation>
+                        )}
+                    </div>      
         </section>
     )
 }
